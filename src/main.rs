b756df8894f7c4a7ee7146a6f39d7877286c8219
Ladislav Hano <524934@mail.muni.cz>
2023-07-07 10:55:59 +0200

    commit ig

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-extern crate dotenv;
+use std::sync::Arc;
 
-use std::env;
-
+use poise::serenity_prelude::GuildChannel;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+use serenity::model::id::ChannelId;
 use serenity::prelude::*;
+use util::security::dotenv_var;
+use serenity::http::{self, Http};
 
-use dotenv::dotenv;
+mod util;
 
 struct Handler;
 
@@ -21,25 +23,50 @@ impl EventHandler for Handler {
         }
     }
 
-    async fn ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
+        // if (ready.user.name != "MOOver Debug") {
+        let messages = ["AAAAAAAAAAAAAAAAAAAA", "Henlooo", "Good day y'all!",
+                        "May have crashed...", "MOOOooo", "Heyyyyy!", "I'm baaaaack!",
+                        "Whom'st have summoned the ancient one?"];
+        
+        // }
+        let opt_token = dotenv_var("TOKEN");
+        if opt_token.is_none() {
+            return;
+        }
+
+        let token = opt_token.unwrap();
+        let channel_result = ctx.http.get_channel(780439236867653635).await;
+        let channel = channel_result.unwrap();
+        
+        // let channel = await http.get_channel(780439236867653635);
+        // GuildChannel::say(&self, http, content)
+        // self.message(ctx, new_message)
+        // C = ;
+        
+        // const debug_channel = 
     }
 }
 
+
+
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
-    let token;
-    let key = "TOKEN";
-    match env::var(key) {
-        Ok(val) => token = val,
-        Err(_) => return,
+    let opt_token = dotenv_var("TOKEN");
+    if opt_token.is_none() {
+        return;
     }
+
+    let token = opt_token.unwrap();
+
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
     
-    let mut client = Client::builder(&token, intents).event_handler(Handler).await.expect("Error creating client");
+    let mut client = Client::builder(&token, intents)
+                                .event_handler(Handler)
+                                .await.expect("Error creating client");
 
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
---
+++ src/util/security.rs
@@ -0,0 +1,12 @@
+use dotenv::dotenv;
+use std::env;
+
+pub fn dotenv_var(key: &str) -> Option<String> {
+    dotenv().ok();
+    let key = "TOKEN";
+
+    match env::var(key) {
+        Ok(val) => return Some(val),
+        Err(_) => None,
+    }
+}
