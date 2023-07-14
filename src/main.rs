9f02f554790b2ceef58a616dff482f44f0eb0f47
Djkato <djkatovfx@gmail.com>
2023-07-14 16:41:16 +0200

    Cleanup :)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use moove::moove;
+use rand::random;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 use util::security::dotenv_var;
-use rand::random;
 
 mod moove;
 mod util;
@@ -13,6 +14,10 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
+        match moove(ctx.http.clone(), msg.clone()).await {
+            Ok(_) => (),
+            Err(e) => println!("ERROR MOVING!{e}"),
+        }
         if msg.content == "!ping" {
             if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
                 println!("Error sending message: {:?}", why);
@@ -24,7 +29,7 @@ impl EventHandler for Handler {
         println!("{} is connected!", ready.user.name);
         let debug = match dotenv_var("DEBUG") {
             Some(v) => v,
-            None => "OFF".to_string()
+            None => "OFF".to_string(),
         };
         if debug == "ON" {
             let messages = [
@@ -36,13 +41,13 @@ impl EventHandler for Handler {
                 "Heyyyyy!",
                 "I'm baaaaack!",
                 "Whom'st have summoned the ancient one?",
-                ];
-            
+            ];
+
             let rand_num = random::<usize>() % messages.len();
             let channel = ctx.http.get_channel(780439236867653635).await.unwrap().id();
             match channel.say(&ctx.http, messages[rand_num]).await {
                 Err(e) => println!("Something went wrong: {e}"),
-                Ok(_) => return
+                Ok(_) => return,
             };
         }
     }
@@ -52,14 +57,13 @@ impl EventHandler for Handler {
 async fn main() -> anyhow::Result<()> {
     use anyhow::Context;
     let token = dotenv_var("TOKEN").context("No TOKEN in env")?;
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
-    
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
         .await
         .context("Failed to build client")?;
-    
+
     client.start().await?;
     Ok(())
 }
